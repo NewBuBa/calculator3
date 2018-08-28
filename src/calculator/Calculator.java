f4f38aacabc3e607ae92cb5554b9fc6ba8a8eb82
java_tsi08 <artjom.cerkasskijs@gmail.com>
2018-08-28 18:49:54 +0300

    29_08

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,39 +6,24 @@ package calculator;
  * @since 1.0
  */
 class Calculator {
-
-    String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        int a = Integer.parseInt(expression[0]);
-        int b = Integer.parseInt(expression[2]);
-        int result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            default:
-                return "ERROR";
-        }
-        if (expression.length < 4) {
+    String calculate(String[] expresion) {
+        double result = Double.parseDouble(expresion[0]);
+        for (int i = 1; i < expresion.length; i += 2) {
+            String op = expresion[i];
+            double b = Double.parseDouble(expresion[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
+            }
             return String.valueOf(result);
         }
-        int c = Integer.parseInt(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            default:
-                return "ERROR";
-        }
-        return String.valueOf(result);
+
     }
 
-}
